83c6843506f8e161307787dab4b798c45cf52ca9
Samidh Patel <spatel13456@gmail.com>
2019-01-10 22:19:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  return attendees.map {|badges| badge_maker(badges)}
+end
+
+def assign_rooms(array)
+  
+
